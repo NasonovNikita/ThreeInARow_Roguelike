cec633da66a4cbfcc5386d5aa3feb3fa70b990e0
nosokvkokose <Cyclist2006@yandex.ru>
2022-10-30 19:53:51 +0300

    Grid
    Grid generator

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    public int size = 10;
+    [SerializeField]
+    private int SizeX;
+    [SerializeField]
+    private int SizeY;
     [SerializeField]
     private Vector2 position;
-
     public GridState State;
-    void Awake()
+    [SerializeField]
+    private Vector2 StepX;
+    [SerializeField]
+    private Vector2 StepY;
+    [SerializeField]
+    private GameObject[] prefabs = new GameObject[4];
+
+    private void Awake()
     {
-        State = GridState.choosing1;
+        State = GridState.refreshing;
         transform.position = position;
+        GenGems();
     }
 
+    private GameObject GenGem(Vector2 position) {
+        GameObject gem = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
+        gem.transform.position = position;
+        return gem;
+    }
+
+    public void GenGems() {
+        GameObject[,] box = new GameObject[SizeY, SizeX];
+        for (int i = 0; i < SizeY; i++) {
+            for (int j = 0; j < SizeX; j++) {
+                box[i, j] = GenGem((Vector2)transform.position + StepX * j + StepY * i);
+            }
+        }
+    }
 }
